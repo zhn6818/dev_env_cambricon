7f000a55862b1a728d16a8a4724a2ef1794037f0
张海宁 <zhn6818@163.com>
2023-01-04 15:30:08 +0800

    update

--- include/cnrt_virgo.h
+++ include/cnrt_virgo.h
@@ -11,18 +11,20 @@
 #include <sstream>
 #include "cnrt.h"
 #include <mm_runtime.h>
+#include <opencv2/opencv.hpp>
 
 using namespace magicmind;
 
 namespace ASCEND_VIRGO
 {
+    typedef std::pair<std::string, float> Predictioin;
     class ClassifyPrivate;
     class Classify
     {
     public:
         Classify(const std::string &model_path, const std::string &name_Path, size_t deviceId, std::string jsonPath = "");
         ~Classify();
-
+        void process(std::vector<cv::Mat> &vecMat, std::vector<Predictioin> &result);
         size_t GetBatch();
         size_t GetInputSize();
 
--- test/test/test.cpp
+++ test/test/test.cpp
@@ -4,8 +4,10 @@ using namespace ASCEND_VIRGO;
 int main()
 {
     std::shared_ptr<Classify> dffg = std::make_shared<Classify>("/data1/qwy/code/magicmind_cloud-master/buildin/cv/classification/resnet50_onnx/data/models/resnet50_onnx_model_force_float32_true_1", "", 0);
-    std::vector<cv::Mat> sdf;
 
+    cv::Mat img = cv::imread("/data1/qwy/dataset/ILSVRC2012/ILSVRC2012_val_00000001.JPEG");
+    std::vector<cv::Mat> sdf;
+    sdf.push_back(img);
     std::vector<Predictioin> df;
     dffg->process(sdf, df);
     return 0;
